7c11e1aebd52a7a4117be0a4e9ed2f807f5eee12
Rafael Martínez Reyes <rterranova701@gmail.com>
2021-10-01 22:03:35 -0500

    Fix prompts

--- module-js/js/02-conditionals/script.js
+++ module-js/js/02-conditionals/script.js
@@ -29,9 +29,11 @@
 // const resultado = age >= 18 ? 'Es adulto': 'no es adulto';
 // console.log(resultado);
 
+
+
+
 // //Numeros pares
-// document.write("Escribe un número del 1 al 100:",'<BR/>');
-// let numero = Number(prompt());
+// let numero = Number(prompt('Escribe un número del 1 al 100'));
 
 // if (numero >= 1 && numero <= 100 ){
 //     if (numero%2 == 0){
@@ -44,11 +46,9 @@
 // }
 
 // //numero mayor
-// document.write("Escribe número 1:",'<BR/>');
-// let num1 = Number(prompt());
+// let num1 = Number(prompt("Escribe número 1:"));
 
-// document.write("Escribe número 2:",'<BR/>');
-// let num2 = Number(prompt());
+// let num2 = Number(prompt("Escribe número 2:"));
 
 // if (num1 == num2){
 //     console.log('Los números son iguales');
@@ -59,11 +59,9 @@
 // }
 
 // //Peso
-// document.write("Escribe tu nombre:",'<BR/>');
-// let name = (prompt());
+// let name = (prompt("Escribe tu nombre:"));
 
-// document.write("Escribe tu peso:",'<BR/>');
-// let peso = Number(prompt());
+// let peso = Number(prompt("Escribe tu peso:"));
 
 // let pesoLuna = peso / 6
 
@@ -71,8 +69,7 @@
 
 
 //calificaciones
-document.write("Escribe un número del 0 al 100:",'<BR/>');
-let numero = Number(prompt());
+let numero = Number(prompt("Escribe un número del 0 al 100:"));
 
 if (numero >= 0 && numero <= 100 ){
     if(numero >= 90){
