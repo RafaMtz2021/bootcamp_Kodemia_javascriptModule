daabddae04873bdab75434e921e38ba7dc52efca
Rafael Martínez Reyes <rterranova701@gmail.com>
2021-10-01 21:37:52 -0500

    Add validation for 0 in lasta exercise

--- module-js/js/02-conditionals/script.js
+++ module-js/js/02-conditionals/script.js
@@ -29,52 +29,52 @@
 // const resultado = age >= 18 ? 'Es adulto': 'no es adulto';
 // console.log(resultado);
 
-//Numeros pares
-document.write("Escribe un número del 1 al 100:",'<BR/>');
-let numero = Number(prompt());
+// //Numeros pares
+// document.write("Escribe un número del 1 al 100:",'<BR/>');
+// let numero = Number(prompt());
 
-if (numero >= 1 && numero <= 100 ){
-    if (numero%2 == 0){
-        console.log('El número es par');
-    }else{
-        console.log('El número es impar');
-    }
-}else{
-    console.log('Número inválido');
-}
+// if (numero >= 1 && numero <= 100 ){
+//     if (numero%2 == 0){
+//         console.log('El número es par');
+//     }else{
+//         console.log('El número es impar');
+//     }
+// }else{
+//     console.log('Número inválido');
+// }
 
-//numero mayor
-document.write("Escribe número 1:",'<BR/>');
-let num1 = Number(prompt());
+// //numero mayor
+// document.write("Escribe número 1:",'<BR/>');
+// let num1 = Number(prompt());
 
-document.write("Escribe número 2:",'<BR/>');
-let num2 = Number(prompt());
+// document.write("Escribe número 2:",'<BR/>');
+// let num2 = Number(prompt());
 
-if (num1 == num2){
-    console.log('Los números son iguales');
-}else if(num1 > num2){
-    console.log(`El número ${num1} es mayor que ${num2}`);
-}else{
-    console.log(`El número ${num2} es mayor que ${num1}`);
-}
+// if (num1 == num2){
+//     console.log('Los números son iguales');
+// }else if(num1 > num2){
+//     console.log(`El número ${num1} es mayor que ${num2}`);
+// }else{
+//     console.log(`El número ${num2} es mayor que ${num1}`);
+// }
 
-//Peso
-document.write("Escribe tu nombre:",'<BR/>');
-let name = (prompt());
+// //Peso
+// document.write("Escribe tu nombre:",'<BR/>');
+// let name = (prompt());
 
-document.write("Escribe tu peso:",'<BR/>');
-let peso = Number(prompt());
+// document.write("Escribe tu peso:",'<BR/>');
+// let peso = Number(prompt());
 
-let pesoLuna = peso / 6
+// let pesoLuna = peso / 6
 
-console.log(`Tu peso en la luna sería de ${pesoLuna} kg`);
+// console.log(`Tu peso en la luna sería de ${pesoLuna} kg`);
 
 
 //calificaciones
-document.write("Escribe un número del 1 al 100:",'<BR/>');
+document.write("Escribe un número del 0 al 100:",'<BR/>');
 let numero = Number(prompt());
 
-if (numero >= 1 && numero <= 100 ){
+if (numero >= 0 && numero <= 100 ){
     if(numero >= 90){
         console.log('A');
     }else if(numero >= 80 && numero < 90){
