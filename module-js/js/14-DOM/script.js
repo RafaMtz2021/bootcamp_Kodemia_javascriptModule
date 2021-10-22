b922cf63e8367102a992588f2ad5411cf8703600
Rafael Martínez Reyes <rterranova701@gmail.com>
2021-10-22 15:30:44 -0500

    Intro DOM

--- module-js/js/14-DOM/script.js
+++ module-js/js/14-DOM/script.js
@@ -156,106 +156,220 @@ const mentorsArray = [
   btn0 = document.querySelector("#B0")
   btn0.addEventListener('click',()=>{
       console.log('click en boton 0');
-      alert('¿Estás seguro de borrar el registro?')
+      alert('El registro será eliminado')
       const item0 = document.querySelector('.R0')
       item0.remove()
-      getAverageBySignature();
+      getAverageBySignature2();
   })
 
   btn1 = document.querySelector("#B1")
   btn1.addEventListener('click',()=>{
       console.log('click en boton 1');
-      alert('¿Estás seguro de borrar el registro?')
+      alert('El registro será eliminado')
       const item1 = document.querySelector('.R1')
       item1.remove()
-      getAverageBySignature();
+      getAverageBySignature2();
   })
 
   btn2 = document.querySelector("#B2")
   btn2.addEventListener('click',()=>{
       console.log('click en boton 2');
-      alert('¿Estás seguro de borrar el registro?')
+      alert('El registro será eliminado')
       const item2 = document.querySelector('.R2')
       item2.remove()
-      getAverageBySignature();
+      getAverageBySignature2();
   })
 
   btn3 = document.querySelector("#B3")
   btn3.addEventListener('click',()=>{
       console.log('click en boton 3');
-      alert('¿Estás seguro de borrar el registro?')
+      alert('El registro será eliminado')
       const item3 = document.querySelector('.R3')
       item3.remove()
-      getAverageBySignature();
+      getAverageBySignature2();
   })
 
 
     //Footer
     const rowTableFooter = document.createElement('tr');
+    rowTableFooter.className = 'R4';
     const thFooter = document.createElement('th');
     const textFooter = document.createTextNode('Averages');
     thFooter.appendChild(textFooter);
     rowTableFooter.appendChild(thFooter);
  
+    table.appendChild(rowTableFooter);
 
 
-
-// const pater = document.querySelector('.R3')
-// console.log(pater.firstChild.nextSibling.textContent);
-
-const getAverageBySignature = ()=>{
+const getAverageBySignature1 = ()=>{
+    //array temporal para guardar scores
     let arrTmp = [];
-    for(let i=0;i<4;i++){
+
+    //idica el tamaño del array en todo momento
+    let counter =0;
+    for(let j=0;j<4;j++){
+        if(document.querySelector('.R'+j)){
+            counter ++
+        }
+    }
+    console.log(counter);
+
+    //HTML
+    for(let i=0;i<counter+1;i++){
         const html = document.querySelector('.C' + i)
-        arrTmp.push(Number(html.textContent))
+        if(html!=null){
+            arrTmp.push(Number(html.textContent))
+        }
     }
     const tdHtml = document.createElement('th');
-    const avgHtml = document.createTextNode(arrTmp.reduce((acum,item)=>acum + item / arrTmp.length ,0))
-    tdHtml.appendChild(avgHtml)
+    const avgHtml = arrTmp.reduce((acum,item)=>acum + item / counter ,0)
+    let txtHtml = document.createTextNode(avgHtml)
+
+    tdHtml.appendChild(txtHtml)
     rowTableFooter.appendChild(tdHtml);
 
+    //CSS
     arrTmp = [];
-    for(let i=0;i<4;i++){
+    for(let i=0;i<counter+1;i++){
         const css = document.querySelector('.C' + i)
-        arrTmp.push(Number(css.nextSibling.textContent))
+        if(css!=null){
+            arrTmp.push(Number(css.nextSibling.textContent))
+        }
     }
     const tdCss = document.createElement('th');
-    const avgCss = document.createTextNode(arrTmp.reduce((acum,item)=>acum + item / arrTmp.length ,0))
-    tdCss.appendChild(avgCss)
+    const avgCss = arrTmp.reduce((acum,item)=>acum + item / counter ,0)
+    let  txtCss = document.createTextNode(avgCss)
+    
+    tdCss.appendChild(txtCss)
     rowTableFooter.appendChild(tdCss);
-
+    //JS
     arrTmp = [];
-    for(let i=0;i<4;i++){
+    for(let i=0;i<counter+1;i++){
         const js = document.querySelector('.C' + i)
-        arrTmp.push(Number(js.nextSibling.nextSibling.textContent))
+        if(js!=null){
+            arrTmp.push(Number(js.nextSibling.nextSibling.textContent))
+        }
     }
     const tdJs = document.createElement('th');
-    const avgJs = document.createTextNode(arrTmp.reduce((acum,item)=>acum + item / arrTmp.length ,0))
-    tdJs.appendChild(avgJs)
+    const avgJs = arrTmp.reduce((acum,item)=>acum + item / counter ,0)
+    let txtJs = document.createTextNode(avgJs)
+    
+    tdJs.appendChild(txtJs)
     rowTableFooter.appendChild(tdJs);
-
+    //REACT JS
     arrTmp = [];
-    for(let i=0;i<4;i++){
+    for(let i=0;i<counter+1;i++){
         const reactJs = document.querySelector('.C' + i)
-        arrTmp.push(Number(reactJs.nextSibling.nextSibling.nextSibling.textContent))
+        if(reactJs!=null){
+            arrTmp.push(Number(reactJs.nextSibling.nextSibling.nextSibling.textContent))
+        }
     }
     const tdReactJs = document.createElement('th');
-    const avgReactJs = document.createTextNode(arrTmp.reduce((acum,item)=>acum + item / arrTmp.length ,0))
-    tdReactJs.appendChild(avgReactJs)
+    const avgReactJs = arrTmp.reduce((acum,item)=>acum + item / counter ,0)
+    let txtReactJs = document.createTextNode(avgReactJs)
+    
+    tdReactJs.appendChild(txtReactJs)
     rowTableFooter.appendChild(tdReactJs);
-
+    //AVERAGE OF AVERAGES
     arrTmp = [];
-    for(let i=0;i<4;i++){
+    for(let i=0;i<counter+1;i++){
         const totalAvg = document.querySelector('.C' + i)
-        arrTmp.push(Number(totalAvg.nextSibling.nextSibling.nextSibling.nextSibling.textContent))
+        if(totalAvg!=null){
+            arrTmp.push(Number(totalAvg.nextSibling.nextSibling.nextSibling.nextSibling.textContent))
+        }
     }
     const tdtotalAvg = document.createElement('th');
-    const avgtotalAvg = document.createTextNode(arrTmp.reduce((acum,item)=>acum + item / arrTmp.length ,0))
-    tdtotalAvg.appendChild(avgtotalAvg)
+    const avgtotalAvg = arrTmp.reduce((acum,item)=>acum + item / counter ,0)
+    let txtTotalAvg = document.createTextNode(avgtotalAvg)
+    
+    tdtotalAvg.appendChild(txtTotalAvg)
     rowTableFooter.appendChild(tdtotalAvg);
-
 }
-getAverageBySignature();
+getAverageBySignature1();
+
+const getAverageBySignature2 = ()=>{
+    //array temporal para guardar scores
+    let arrTmp = [];
+
+    //idica el tamaño del array en todo momento
+    let counter = 0;
+    for(let j=0;j<4;j++){
+        if(document.querySelector('.R'+j)){
+            counter ++
+        }
+    }
+    console.log(counter);
+
+    //HTML
+    for(let i=0;i<=counter;i++){
+        let html = document.querySelector('.C' + i)
+        if(html===null){
+            continue
+        }else{
+            arrTmp.push(Number(html.textContent))
+        }
+    }
+    console.log(arrTmp);
+    const avgHtml = arrTmp.reduce((acum,item)=>acum + item / counter ,0).toFixed(2);
+    console.log(avgHtml);
 
 
-table.appendChild(rowTableFooter);
+    //CSS
+    arrTmp = [];
+    for(let i=0;i<=counter;i++){
+        let css = document.querySelector('.C' + i)
+        if(css===null){
+            console.log(css);
+            continue
+        }else{
+            arrTmp.push(Number(css.nextSibling.textContent))
+        }
+    }
+    console.log(arrTmp);
+    const avgCss = arrTmp.reduce((acum,item)=>acum + item / counter ,0).toFixed(2);
+    console.log(avgCss);
+
+    //JS
+    arrTmp = [];
+    for(let i=0;i<=counter;i++){
+        let js = document.querySelector('.C' + i)
+        if(js===null){
+            continue
+        }else{
+            arrTmp.push(Number(js.nextSibling.nextSibling.textContent))
+        }
+    }
+    console.log(arrTmp);
+    const avgJs = arrTmp.reduce((acum,item)=>acum + item / counter ,0).toFixed(2);
+
+    console.log(avgJs);
+
+    //REACT JS
+    arrTmp = [];
+    for(let i=0;i<=counter;i++){
+        let reactJs = document.querySelector('.C' + i)
+        if(reactJs===null){
+            continue
+        }else{
+            arrTmp.push(Number(reactJs.nextSibling.nextSibling.nextSibling.textContent))
+        }
+    }
+    console.log(arrTmp);
+    const avgReactJs = arrTmp.reduce((acum,item)=>acum + item / counter ,0).toFixed(2);
+
+    console.log(avgReactJs);
+
+    //AVERAGE OF AVERAGES
+    arrTmp = [];
+    for(let i=0;i<=counter;i++){
+        let totalAvg = document.querySelector('.C' + i)
+        if(totalAvg===null){
+            continue
+        }else{
+            arrTmp.push(Number(totalAvg.nextSibling.nextSibling.nextSibling.nextSibling.textContent))
+        }
+    }
+    console.log(arrTmp);
+    const avgtotalAvg = arrTmp.reduce((acum,item)=>acum + item / counter ,0).toFixed(2);
+    console.log(avgtotalAvg);
+}
