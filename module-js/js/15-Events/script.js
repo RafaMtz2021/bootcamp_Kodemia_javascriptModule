53d9c239162de02af2fcb2049cd1bb3f7edcdf84
Rafael Martínez Reyes <rterranova701@gmail.com>
2021-10-26 16:44:42 -0500

    DOM events

---
+++ module-js/js/15-Events/script.js
@@ -0,0 +1,13 @@
+const plusBtn = document.querySelector('.add');
+const minusBtn = document.querySelector('.minus');
+const text = document.querySelector('.text')
+
+plusBtn.addEventListener('click',()=>{
+    let contador = 0;
+    text.value ++
+})
+
+minusBtn.addEventListener('click',()=>{
+    let contador = 0;
+    text.value --
+})
