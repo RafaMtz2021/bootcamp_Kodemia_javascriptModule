fa1c779c7d5a41c84fe7b6db266ed90026ca375d
Rafael Martínez Reyes <rterranova701@gmail.com>
2021-10-29 14:48:32 -0500

    Jquery homework

---
+++ module-js/js/18-JQUERY/script.js
@@ -0,0 +1,34 @@
+$(document).ready( () => {
+    //Agrega texto al Post It
+    const addText = ()=>{
+        const text = $('#text').val();
+        text !== '' ? createPostIt(text.toUpperCase().trim()):
+        alert('Ingresa un texto');
+    }
+    //Crea Post It
+    let id = 1;
+    const createPostIt = (text) => {
+        const square = document.createElement('div');
+        square.className = 'postIt m-3 col-4';
+        square.id = id++;
+        square.textContent = text;
+        $('#draw').append(square);
+    };
+    //Detecta cualquier cambio en el DOM y pinta el Post It
+    $('#draw').bind('DOMSubtreeModified', () => {
+        //console.log('changed');
+        const item = $('.postIt')
+        $(item).click( (e) => {
+            const id = e.target.id
+            //console.log(id);
+            $(item).removeClass('colorize');
+            $('#'+id).addClass('colorize');
+        });
+      });
+    //Agrega texto y crea Post It 
+    $('#addItem').click( () => { 
+        addText();
+    });
+});
+
+
